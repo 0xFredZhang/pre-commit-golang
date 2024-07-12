9f8b1f6855d0f39df1110691eb372b54ff52ad20
0xFredZhang <142585051+0xFredZhang@users.noreply.github.com>
2024-07-12 17:19:56 +0800

    Update run-go-build.sh

--- run-go-build.sh
+++ run-go-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 FILES=$(go list ./cmd/...  | grep -v /vendor/)
-exec CGO_LDFLAGS="-Wl,-no_warn_duplicate_libraries" go build $FILES
+exec 'CGO_LDFLAGS="-Wl,-no_warn_duplicate_libraries" go build $FILES'
